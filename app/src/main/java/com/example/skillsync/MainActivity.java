bfa27dbd3c5edb617c785936e3d11a0d0e44611a
Tejashwi C <tejashwi46@gmail.com>
2026-05-11 22:33:11 +0530

    Add android files

---
+++ app/src/main/java/com/example/skillsync/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.skillsync;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl("https://my-skillsync.netlify.app/");
+    }
+}
